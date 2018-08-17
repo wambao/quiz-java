efc97e9acea256f7ebacd5deffc318aea00ed2dd
LcNdinda <lcndinda@gmail.com>
2018-08-17 12:24:16 +0300

    Add var Que1 and Que2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
+var testResults = function(one, two){
+return one + two;
+
+
+}
+
 $(document).ready(function(){
   $(".start").click(function(){
-    ("#test").fadeIn("slow");
+    $("#test").fadeToggle("slow");
+  });
+
+  $("form#test").submit(function(event){
+    var Que1 = parseInt($("input:radio[name=Q1]:checked").val());
+    var Que2 = parseInt($("input:radio[name=Q2]:checked").val());
+    var result = testResults(Que1, Que2);
+
+    $("#display").text("Your score is:" + result);
+
+    event.preventDefault();
+    $(".finalResult").show();
   });
 });
