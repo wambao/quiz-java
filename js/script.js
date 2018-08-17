0923c40ea587259b8876655b9e0781259cf8f929
LcNdinda <lcndinda@gmail.com>
2018-08-17 12:58:00 +0300

    Add a function testResult

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var testResults = function(one, two){
-return one + two;
+var testResults = function(one, two ,three, four, five){
+return one + two + three + four + five;
 
 
 }
@@ -12,7 +12,10 @@ $(document).ready(function(){
   $("form#test").submit(function(event){
     var Que1 = parseInt($("input:radio[name=Q1]:checked").val());
     var Que2 = parseInt($("input:radio[name=Q2]:checked").val());
-    var result = testResults(Que1, Que2);
+    var Que3 = parseInt($("input:radio[name=Q3]:checked").val());
+    var Que4 = parseInt($("input:radio[name=Q4]:checked").val());
+    var Que5 = parseInt($("input:radio[name=Q5]:checked").val());
+    var result = testResults(Que1, Que2, Que3, Que4, Que5);
 
     $("#display").text("Your score is:" + result);
 
