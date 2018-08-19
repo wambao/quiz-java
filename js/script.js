e2973d10e1f6d1932f4dfb6be903dbab0d49eae9
LcNdinda <lcndinda@gmail.com>
2018-08-19 06:38:29 +0300

    Addaddclass() and removeClass() to body

--- js/script.js
+++ js/script.js
@@ -1,25 +1,68 @@
 var testResults = function(one, two ,three, four, five){
-return one + two + three + four + five;
+  // if (isNaN(one) || isNaN(two) || isNaN(three) ||   isNaN(four) || isNaN(four) ) {
+  //   alert("Check the answers to your question");
+  //
+  //
+  //
+  // }
+  //
+  // else {
+      return one + two + three + four + five;
+//  }
 
 
-}
+};
 
 $(document).ready(function(){
   $(".start").click(function(){
-    $("#test").fadeToggle("slow");
+    $("#test").fadeIn("slow");
+    $("#intro").fadeOut("slow");
+    $(".panel").fadeOut("slow");
+
+  });
+
+  $("button#grey").click(function(){
+    $("body").removeClass();
+    $("body").addClass("grey-background");
+  });
+
+  $("button#green").click(function(){
+    $("body").removeClass();
+    $("body").addClass("green-background");
   });
 
   $("form#test").submit(function(event){
-    var Que1 = parseInt($("input:radio[name=Q1]:checked").val());
-    var Que2 = parseInt($("input:radio[name=Q2]:checked").val());
-    var Que3 = parseInt($("input:radio[name=Q3]:checked").val());
-    var Que4 = parseInt($("input:radio[name=Q4]:checked").val());
-    var Que5 = parseInt($("input:radio[name=Q5]:checked").val());
-    var result = testResults(Que1, Que2, Que3, Que4, Que5);
+    var que1 = parseInt($("input:radio[name=Q1]:checked").val());
+     if($("input:radio[name=Q1]:checked").length == 0){
+       $('#q1').text("An answer is required");
+               return false;
+     }
+    var que2 = parseInt($("input:radio[name=Q2]:checked").val());
+    if($("input:radio[name=Q2]:checked").length == 0){
+      $('#q2').text("An answer is required");
+              return false;
+    }
+    var que3 = parseInt($("input:radio[name=Q3]:checked").val());
+    if($("input:radio[name=Q3]:checked").length == 0){
+      $('#q3').text("An answer is required");
+              return false;
+    }
+    var que4 = parseInt($("input:radio[name=Q4]:checked").val());
+    if($("input:radio[name=Q4]:checked").length == 0){
+      $('#q4').text("An answer is required");
+              return false;
+    }
+    var que5 = parseInt($("input:radio[name=Q5]:checked").val());
+    if($("input:radio[name=Q5]:checked").length == 0){
+      $('#q5').text("An answer is required");
+              return false;
+    }
+    var result = testResults(que1, que2, que3, que4, que5);
 
-    $("#display").text("Your score is:" + result);
+    $("#display").text("Your score is: " + result + " /25");
 
     event.preventDefault();
+    $("#test").fadeOut();
     $(".finalResult").show();
   });
 });
